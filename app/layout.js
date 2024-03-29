7da2474226cac412ef3500d82084762202f14743
HafizMSubhan <hafiz.muhammad.subhan@vaival.com>
2024-03-29 12:09:32 +0500

    V1.2

--- app/layout.js
+++ app/layout.js
@@ -11,11 +11,15 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Hafiz Subhan',
   description: 'This is the portfolio of Hafiz Subhan. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+  
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
